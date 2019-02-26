b6fc8317450049e553f3d137490bda94f185010f
dush1729 <dush1729@gmail.com>
2019-02-27 00:11:37 +0530

    Stash commit

--- main.py
+++ main.py
@@ -4,12 +4,35 @@ import pymsgbox
 import time
 import playsound
 
-handle = 'dush1729'
+handle = 'div24ever'
 URL = 'https://codeforces.com/api/user.rating?handle=' + handle
 
 data = json.load(urllib.request.urlopen(URL))
 contestList = data["result"]
 initialContestCount = len(contestList)
+
+def displayMesage(message):
+	pymsgbox.alert(message)
+
+def playSound(filename):
+	playsound.playsound(filename)
+
+def alertUser(oldRating, newRating):
+	message,filename = "", ""
+	if oldRating < newRating:
+		message = "Rating incresed by " + str(newRating - oldRating)
+		filename = 'assets/win.mp3'
+	elif oldRating == newRating:
+		message = "No rating change"
+	else:
+		message = "Rating decreased by " + str(oldRating - newRating)
+		filename = 'assets/lose.mp3'
+	p1 = Process(target = displayMesage(message))
+	p1.start()
+	p2 = Process(target = playSound(filename))
+	p2.start()
+	p1.join()
+	p2.join()	
 
 while True:
 	data = json.load(urllib.request.urlopen(URL))
